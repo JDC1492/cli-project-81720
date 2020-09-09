f9eda5387b725c9d3e618f25c2748c4c4ab82e92
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-09-09 12:07:59 -0400

    class, variable name change

--- lib/plant.rb
+++ lib/plant.rb
@@ -1,5 +1,16 @@
 class Plant
 
+    @@all=[]
+    attr_accessor :common_name
+
+    def initialize(name)
+        self.new = name[:common_name]
+    end
+
+    def self.all
+        @@all
+    end
+
 
     
 end
