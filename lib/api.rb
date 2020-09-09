6a1613b3d823830cec94c78f63826a5cba13026f
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-09-09 11:43:07 -0400

    checking trefle data

---
+++ lib/api.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+# class API
+#     def self.get
+#     resp = RestClient.get('https://api.jikan.moe/v3')
+#     end
+#     binding.pry
+# end
+
+
+
+#176181200660885/character-id
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,6 @@
 class CLI
-    
+    def greet
+        puts "Welcome to the D.A.R.K. training facility. Whom do you seek knowledge of?"
+    end
     
 end
