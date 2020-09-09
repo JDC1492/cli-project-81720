b7dfffee65feff4191a00ebfb1de91168f35365f
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-09-09 14:42:37 -0400

    cli change to greet

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,7 @@
 class CLI
     def greet
-        puts "Welcome to the D.A.R.K. training facility. Whom do you seek knowledge of?"
+        puts "Welcome to the Greenhouse!"
+        puts "What plant information are you looking for?"
     end
     
 end
