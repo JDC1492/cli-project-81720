5b09797b30f3c6df49a290885ea37091a8da7bef
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-09-10 15:00:49 -0400

    ending the program with the use of exit AFTER a short poem

--- lib/cli.rb
+++ lib/cli.rb
@@ -29,10 +29,36 @@ class CLI
             plant = Plants.all[input.to_i-1]
             display_plant_info(plant)
         end
+        #to keep the flow, I'm asking the user 
+        #would they like to find out more about another plant option.
+        puts "Eager to learn a few more facts about plants?"
+        puts "Enter Y or N"
+        anot_select = gets.strip.downcase
+        if anot_select == "y"
+            #again calling the methods to give the user 
+            #the list of options and chance to input
+            list_plants
+            input
+        else 
+            #closing the project with a corny poem
+            puts "Hate to see you go..."
+            puts " but with the knowledge you now know..."
+            puts "  go be free, plant a tree..."
+            puts "   be at peace while you watch it grow..."
+            sleep(0.3)
+            exit
+        end
+
     end
 
     def display_plant_info(plant)
         API.scrape_indi_plants(plant)
+        puts "Just some minor details about the #{plant.name}"
+        puts "Scientific Name: #{plant.scientific_name}" 
+        puts "Genus: #{plant.genus}"
+        puts "Observations: #{plant.observations}"
+        puts "Family: #{plant.family}"
+        puts "Edible: #{plant.edible}"
     end
 
 end
